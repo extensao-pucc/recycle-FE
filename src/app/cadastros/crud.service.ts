6508507fd335d565ad5384fbd443d307f8cb12e5
c-Bruno <ribeiro.caboclo@gmail.com>
2020-11-03 19:11:55 -0300

    Validacao de email, cep, telefone, cpf/cnpj, isempty

--- src/app/cadastros/crud.service.ts
+++ src/app/cadastros/crud.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
--- src/app/shared/formValidator/form-validator.service.ts
+++ src/app/shared/formValidator/form-validator.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { CrudService } from '../../cadastros/crud.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormValidatorService {
 
-  constructor() { }
+  constructor(
+    private crudService: CrudService,
+  ) { }
 
-  emailDomainValidator(control: FormControl) {
-    const msgErro = 'Este campo ontem A';
-    let str = control.value;
-    if (str && str.includes('a')) {
+  isNumeric(control: FormControl): any {
+    const msgErro = 'Apenas numeros aqui';
+    let field = control.value;
+    if (field && isNaN(field)) {
       return {msgErro};
     }
     return null;
@@ -19,11 +22,172 @@ export class FormValidatorService {
 
   isEmpty(control: FormControl): any {
     const msgErro = 'Obrigatório';
-    let filed = control.value;
+    let field = control.value;
 
-    if (!filed) {
+    if (!field) {
       return {msgErro};
     }
     return null;
   }
+
+  // isUnique(control: FormControl): any {
+  //   const msgErro = 'Esse CPF/CNPJ já existe';
+  //   let field = control.value;
+
+  //   this.crudService.getItems('fornecedores').subscribe(response => {
+
+  //   });
+
+  //   if (field) {
+  //     return {msgErro};
+  //   }
+  //   return null;
+  // }
+
+  validTelefone(control: FormControl): any {
+    const msgErro = 'Telefone inválido';
+    let field = control.value;
+
+    let er = new RegExp(/^[0-9]{10,11}$/);
+
+    if (field &&  !er.test(field) ) {
+      return {msgErro};
+    }
+    return null;
+  }
+
+  validCEP(control: FormControl): any{
+    const msgErro = 'CEP inválido';
+    let field = control.value;
+
+    let er = new RegExp(/^[0-9]{8}$/);
+
+    if (field &&  !er.test(field) ) {
+      return {msgErro};
+    }
+    return null;
+  }
+
+  validEmail(control: FormControl): any{
+    const msgErro = 'Email inválido';
+    let field = control.value;
+
+    let er = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
+
+    if (field &&  !er.test(field) ) {
+      return {msgErro};
+    }
+    return null;
+  }
+
+  validCPF_CNPJ(control: FormControl): any{
+    let numeros, digitos, soma, i, resultado, digitos_iguais;
+    let field = control.value;
+
+    /* ========== CPFs válidos ============
+    -> 052.922.300-73
+    -> 262.496.500-91
+    -> 132.943.360-20
+    -> 00417720084
+    -> 75523042005
+    -> 48405867058
+    ======================================*/
+
+    /*========== CNPJs válidos ============
+    -> 87.513.771/0001-60
+    -> 58.921.440/0001-61
+    -> 72.901.073/0001-65
+    -> 19556729000177
+    -> 49510856000104
+    ======================================*/
+
+
+    // validação CPF
+    if (field && field.length <= 11){
+      const msgErro = 'CPF inválido';
+
+      digitos_iguais = 1;
+      if (field.length < 11) {
+        return {msgErro};
+      }
+
+      for (i = 0; i < field.length - 1; i++) {
+        if (field.charAt(i) != field.charAt(i + 1)){
+          digitos_iguais = 0;
+          break;
+        }
+      }
+
+      if (!digitos_iguais){
+        numeros = field.substring(0, 9);
+        digitos = field.substring(9);
+        soma = 0;
+
+        for (i = 10; i > 1; i--) {
+              soma += numeros.charAt(10 - i) * i;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(0)) {
+          return {msgErro};
+        }
+
+        numeros = field.substring(0, 10);
+        soma = 0;
+        for (i = 11; i > 1; i--) {
+              soma += numeros.charAt(11 - i) * i;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(1)) {
+          return {msgErro};
+        }
+        return null;
+      } else {
+        return {msgErro};
+      }
+
+    }
+    // validação CNPJ
+    else if (field && field.length != 14) {
+      const msgErro = 'CNPJ inválido';
+      if (field.length != 14) {
+        return {msgErro};
+      }
+
+      let tamanho = field.length - 2;
+      numeros = field.substring(0, tamanho);
+      digitos = field.substring(tamanho);
+      soma = 0;
+      let pos = tamanho - 7;
+      for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2) {
+          pos = 9;
+        }
+      }
+      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+      if (resultado != digitos.charAt(0)) {
+        return {msgErro};
+      }
+
+      tamanho = tamanho + 1;
+      numeros = field.substring(0, tamanho);
+      soma = 0;
+      pos = tamanho - 7;
+      for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2) {
+          pos = 9;
+        }
+      }
+      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+      if (resultado != digitos.charAt(1)) {
+        return {msgErro};
+      }
+
+      return null;
+    }
+  }
+
 }
