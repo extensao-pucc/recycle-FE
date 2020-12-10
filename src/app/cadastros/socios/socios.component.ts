dc31b475b36a2ada313943e33b3e91641db458da
c-Bruno <ribeiro.caboclo@gmail.com>
2020-12-09 21:48:42 -0300

    Melhoria mascara CPF_CNPJ e inputs de busca com nova regex

--- src/app/cadastros/socios/socios.component.ts
+++ src/app/cadastros/socios/socios.component.ts
@@ -96,8 +96,8 @@ export class SociosComponent implements OnInit {
 
     if (valor !== ''){
       this.tempItemsList = this.itemsList.filter(res => {
-        return res[campo].toString().trim().toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').match(
-               valor.trim().toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''
+        return res[campo].toString().trim().toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f-\.|\-\(\) '\/]/g, '').match(
+               valor.trim().toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f-\.|\-\(\) '\/]/g, ''
               ));
       });
     } else if (valor === '') {
