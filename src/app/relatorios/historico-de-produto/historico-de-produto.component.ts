f42b00450dadb74fec94e773849ecfcb53edd934
c-Bruno <ribeiro.caboclo@gmail.com>
2021-02-24 18:50:49 -0300

    Puxado produtos na tela de historico

--- src/app/relatorios/historico-de-produto/historico-de-produto.component.ts
+++ src/app/relatorios/historico-de-produto/historico-de-produto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CrudService } from '../../cadastros/crud.service';
 
 @Component({
   selector: 'app-historico-de-produto',
@@ -6,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historico-de-produto.component.css']
 })
 export class HistoricoDeProdutoComponent implements OnInit {
+  produtos: any;
 
-  constructor() { }
+  constructor(
+    private crudService: CrudService,
+    private formBuilder: FormBuilder,
+  ) { }
   public headForm;
   public socios;
 
   ngOnInit(): void {
+    this.getItems();
+    this.loadForm();
+  }
+
+  getItems(): void {
+    this.crudService.getItems('produtos').subscribe(response => { this.produtos = response; });
+  }
+
+  loadForm(): void {
+    this.headForm = this.formBuilder.group({
+      product: [null],
+      inicio: [null],
+      fim: [null]
+    })
   }
 
 }
