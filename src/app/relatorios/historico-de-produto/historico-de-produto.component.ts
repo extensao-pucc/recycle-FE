a75a31fd08cc3df21af2d207e5fa50b6fa0b99b9
c-Bruno <ribeiro.caboclo@gmail.com>
2021-02-24 20:55:51 -0300

    Integrando tela de movimentações com o back

--- src/app/relatorios/historico-de-produto/historico-de-produto.component.ts
+++ src/app/relatorios/historico-de-produto/historico-de-produto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { CrudService } from '../../cadastros/crud.service';
+import { SharedVariableService } from '../../shared/shared-variable.service';
 
 @Component({
   selector: 'app-historico-de-produto',
@@ -9,10 +10,12 @@ import { CrudService } from '../../cadastros/crud.service';
 })
 export class HistoricoDeProdutoComponent implements OnInit {
   produtos: any;
+  movimentacoes: any;
 
   constructor(
     private crudService: CrudService,
     private formBuilder: FormBuilder,
+    private sharedVariableService: SharedVariableService,
   ) { }
   public headForm;
   public socios;
@@ -24,6 +27,7 @@ export class HistoricoDeProdutoComponent implements OnInit {
 
   getItems(): void {
     this.crudService.getItems('produtos').subscribe(response => { this.produtos = response; });
+    this.crudService.getItems('movimentacoes').subscribe(response => { this.movimentacoes = response; });
   }
 
   loadForm(): void {
