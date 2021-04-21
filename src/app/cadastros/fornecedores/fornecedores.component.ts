c187ee7c8b83bef68db091614217d9c584d12b34
c-Bruno <ribeiro.caboclo@gmail.com>
2021-04-20 22:10:11 -0300

    sort por coluna na tabela

--- src/app/cadastros/fornecedores/fornecedores.component.ts
+++ src/app/cadastros/fornecedores/fornecedores.component.ts
@@ -120,7 +120,7 @@ export class FornecedoresComponent implements OnInit {
           this.loadForm();
                     
           this.showForm = false;
-          this.toastService.addToast('Atualizado com sucesso');
+          this.toastService.addToast('Atualizado com sucesso!');
 
           // Atualiza fornecedor da triagem no localstorage
           var triagem = JSON.parse(localStorage.prodInfoHead);
@@ -192,5 +192,53 @@ export class FornecedoresComponent implements OnInit {
       this.itemForm.controls.UF.setValue(item.estado);
     }
     this.itemForm.controls.CEP.setValue(item.cep);
+  }
+
+  sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("myTable");
+    switching = true;
+    dir = "asc"; 
+
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+
+      for (i = 2; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount ++;      
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
   }
 }
--- src/app/cadastros/prensas/prensas.component.ts
+++ src/app/cadastros/prensas/prensas.component.ts
@@ -95,7 +95,7 @@ export class PrensasComponent implements OnInit {
           this.loadForm();
 
           this.showForm = false;
-          this.toastService.addToast('Atualizado com sucesso');
+          this.toastService.addToast('Atualizado com sucesso!');
         }, err => {
           if (err.error.numero){
             this.itemForm.controls.numero.errors = {'msgErro': 'Prensa com esse numero já existe'};
@@ -148,5 +148,53 @@ export class PrensasComponent implements OnInit {
       }
     };
     this.showYesNoMessage = true;
+  }
+
+  sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("myTable");
+    switching = true;
+    dir = "asc"; 
+
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+
+      for (i = 2; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount ++;      
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
   }
 }
